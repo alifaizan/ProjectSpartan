174be84c31224176d7130d678063d6fcc92175db
Zameer <zameer.masjedee@carleton.ca>
2015-10-19 22:31:56 -0400

    Removed Database class from project

--- ProjectSpartan/src/Simulation.java
+++ ProjectSpartan/src/Simulation.java
@@ -4,11 +4,16 @@
 //Version #: 1
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Simulation {
 
+    private List<Document> documents;
+    private List<User> users;
+    private List<String> tags;
+
+
     public static void main(String[] args) {
-        Database database = new Database();
 
     }
 
