844fa92bd33d7fe3a3c56e0d5b59cb7882c1b758
alifaizann <alifaizann95@gmail.com>
2015-11-09 22:51:43 -0500

    Update ConsumerTest.java
    Test

--- ProjectSpartan/src/test/main/java/ConsumerTest.java
+++ ProjectSpartan/src/test/main/java/ConsumerTest.java
@@ -10,18 +10,22 @@ import org.junit.Test;
 
 public class ConsumerTest {
 
+	public Consumer testConsumer;
+	public Document testDocument;
+
     @Before
     public void setUp() throws Exception {
-
+    	testConsumer = new Consumer(new Simulation(), "testName","testTaste");
+    	testDocument = new Document("testDocument", "testTag", new Producer(simulation, "testProducer", "testTag"));
     }
 
     @Test
     public void testAct() throws Exception {
-
+    	assert testConsumer.act() == 0;
     }
 
     @Test
     public void testCalculatePayoff() throws Exception {
-
+    	assert testConsumer.calculatePayoff() == 0;
     }
 }
