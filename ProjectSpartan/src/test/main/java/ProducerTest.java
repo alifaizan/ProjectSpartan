15ffc1db066da0dd9c37d4878cf22a183a8f3dc9
alifaizann <alifaizann95@gmail.com>
2015-11-09 22:57:50 -0500

    Update ProducerTest.java

--- ProjectSpartan/src/test/main/java/ProducerTest.java
+++ ProjectSpartan/src/test/main/java/ProducerTest.java
@@ -9,19 +9,23 @@ import org.junit.Test;
 
 public class ProducerTest {
 
+    public Producer testProducer;
+    public Document testDocument;
+
     @Before
     public void setUp() throws Exception {
-
+        testProducer = new Consumer(new Simulation(), "testName","testTaste");
+        testDocument = new Document("testDocument", "testTag", new Producer(simulation, "testProducer", "testTag"));
     }
 
     @Test
     public void testAct() throws Exception {
-
+        assert testProducer.act() == 0;
     }
 
     @Test
     public void testNewDoc() throws Exception {
-
+        assert testProducer == 0;
     }
 
     @Test
